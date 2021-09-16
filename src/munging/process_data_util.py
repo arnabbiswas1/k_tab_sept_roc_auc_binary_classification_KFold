7478861842278da204bfb3083982cd4af87d12bd
Arnab Biswas <arnabbiswas1@outlook.com>
2021-09-16 18:00:43 +0530

    All featuresa and compression

--- src/munging/process_data_util.py
+++ src/munging/process_data_util.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 __all__ = [
     "read_raw_data",
@@ -8,6 +8,7 @@ __all__ = [
     "change_dtype",
     "map_variable",
     "merge_df",
+    "reduce_memory_usage"
 ]
 
 
@@ -91,7 +92,9 @@ def read_processed_fillna_data(
             test_df = test_df.sample(frac=frac)
         logger.info(f"Shape of test_df : {test_df.shape}")
     if sample_submission:
-        sample_submission_df = pd.read_parquet(f"{data_dir}/sub_processed_fillna.parquet")
+        sample_submission_df = pd.read_parquet(
+            f"{data_dir}/sub_processed_fillna.parquet"
+        )
         if frac is not None:
             sample_submission_df = sample_submission_df.sample(frac=frac)
         logger.info(f"Shape of sample_submission_df : {sample_submission_df.shape}")
@@ -134,3 +137,43 @@ def merge_df(logger, left_df, right_df, how, on):
     logger.info(merged_df.isna().sum())
     logger.info("After merge shape of merged_df {merged_df.shape}")
     return merged_df
+
+
+def reduce_memory_usage(logger, df, verbose=True):
+    numerics = ["int8", "int16", "int32", "int64", "float16", "float32", "float64"]
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if (
+                    c_min > np.finfo(np.float16).min
+                    and c_max < np.finfo(np.float16).max
+                ):
+                    df[col] = df[col].astype(np.float32)
+                elif (
+                    c_min > np.finfo(np.float32).min
+                    and c_max < np.finfo(np.float32).max
+                ):
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    if verbose:
+        logger.info(
+            "Mem. usage decreased to {:.2f} Mb ({:.1f}% reduction)".format(
+                end_mem, 100 * (start_mem - end_mem) / start_mem
+            )
+        )
+    return df
